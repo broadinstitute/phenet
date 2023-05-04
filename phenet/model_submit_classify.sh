a7ee3227eac73b48d7e3ea10b5351e16ef231a6d
curoli <curoli@gmail.com>
2023-05-03 20:17:52 -0400

    Parallel, part 1.

--- phenet/model_submit_classify.sh
+++ phenet/model_submit_classify.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-#############################
-### Default UGER Requests ###
-#############################
+options=$(getopt -l "num:" -o "n:" -a -- "$@")
+eval set -- "$options"
+while true; do
+  case "$1" in
+    -n|--num)
+      shift
+      export num_chunks=$1
+      ;;
+    --)
+      shift
+      break
+      ;;
+  esac
+  shift
+done
 
-# This section specifies uger requests.  
+echo "num_chunks=$num_chunks"
 
-######################
-### Dotkit section ###
-######################
+if [[ $SGE_TASK_ID ]]; then
 
-# This is required to use dotkits inside scripts
-source /broad/software/scripts/useuse
+  ######################
+  ### Dotkit section ###
+  ######################
 
-# Use your dotkit
-reuse Python-3.9
-reuse Anaconda3
+  # This is required to use dotkits inside scripts
+  source /broad/software/scripts/useuse
 
-source activate model
-##################
-### Run script ###
-##################
+  # Use your dotkit
+  reuse Python-3.9
+  reuse Anaconda3
 
-python /humgen/diabetes2/users/oliverr/git/phenet/phenet/multi_fit_new.py classify --config-file /humgen/diabetes2/users/oliverr/git/phenet/cfg/lipo_base2_trained.cfg --pymc3 --debug-level 3 --delim ";" --output-file /humgen/diabetes2/users/oliverr/phenet/out/classifieds
+  source activate model
+  ##################
+  ### Run script ###
+  ##################
+
+  python /humgen/diabetes2/users/oliverr/git/phenet/phenet/multi_fit_new.py classify \
+    --config-file /humgen/diabetes2/users/oliverr/git/phenet/cfg/lipo_base2_trained.cfg --pymc3 --debug-level 3 \
+    --delim ";" --output-file /humgen/diabetes2/users/oliverr/phenet/out/classifieds
+
+else
+    qsub -N your_project_name -l h_vmem=2G -l h_rt=4:00:00 -cwd -j y -o your_log.log -t 1-$num_chunks run.sh
+fi
 
